28a0014154f261305fa43ab33230b5e76cd4fd16
Brainlesslukas <lukas.haible@web.de>
2025-02-05 17:04:00 +0100

    Adding CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:8080', // Vue App
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
