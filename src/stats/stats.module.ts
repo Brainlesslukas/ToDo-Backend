f2451906fae01d4cf4a82c7e649b487770f0c43e
Brainlesslukas <lukas.haible@web.de>
2025-02-12 18:53:11 +0100

    Fixing Import Bug

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -3,9 +3,10 @@ import { StatsController } from './stats.controller';
 import { StatsService } from './stats.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auth } from '../auth/auth.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auth])],
+  imports: [TypeOrmModule.forFeature([Auth]), AuthModule],
   controllers: [StatsController],
   providers: [StatsService],
   exports: [StatsService],
